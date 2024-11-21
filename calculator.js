b305ec0a469b6f288dc45a7cd671bf7941d59d12
Fralop <117324827+Fralop@users.noreply.github.com>
2024-11-20 23:23:09 -0500

    Managed to make the operations work with multiple subsequent operators ! Created a temporary operator which updates when a number is entered.

--- calculator.js
+++ calculator.js
@@ -16,6 +16,8 @@ let op = 0;
 let a = 0;
 let b = 0;
 let finalResult = 0;
+let opCounter = 0;
+let tempOp = 0;
 function operate (a, op, b) {
     let result = 0;
     switch (op) {
@@ -52,6 +54,10 @@ let clicks = [];
 function clickEffect (e) {
     switch (e.target.classList[0]) {
         case "num":
+            if (finalResult !== 0 && opCounter >= 2) {
+                a = finalResult;
+                op = tempOp;
+            }
             clicks.push(e.target.textContent);
             display.textContent = Number(clicks.join(''));
             console.log(e.target.classList[0]);
@@ -61,22 +67,41 @@ function clickEffect (e) {
             console.log(op);
         break;
         case "ope":
-            if (finalResult === 0) {
-               a = Number(clicks.join(''));
-            } else a = finalResult;
-            op = e.target.textContent
-            clicks = [];
-            console.log(e.target.classList[0]);
-            console.log(clicks);
-            console.log(a);
-            console.log(b);
-            console.log(op);
+            opCounter++
+            if (opCounter < 2) {
+                if (finalResult === 0) {
+                a = Number(clicks.join(''));
+                } else a = finalResult;
+                op = e.target.textContent
+                clicks = [];
+                console.log(e.target.classList[0]);
+                console.log(clicks);
+                console.log(a);
+                console.log(b);
+                console.log(op);
+                console.log(finalResult);
+
+            } else {
+                b = Number(clicks.join(''));
+                finalResult = operate (a, op, b);
+                tempOp = e.target.textContent;
+                clicks = [];
+                display.textContent = finalResult;
+                console.log(e.target.classList[0]);
+                console.log(clicks);
+                console.log(a);
+                console.log(b);
+                console.log(op);
+                console.log(finalResult);
+
+            }
         break;
         case "igual":
             b = Number(clicks.join(''));
             finalResult = operate (a, op, b);
             clicks = [];
             display.textContent = finalResult;
+            opCounter = 0;
             console.log(e.target.classList[0]);
             console.log(clicks);
             console.log(a);
@@ -84,6 +109,14 @@ function clickEffect (e) {
             console.log(op);
             console.log(finalResult);
         break;
+        case "AC":
+            op = 0;
+            a = 0;
+            b = 0;
+            finalResult = 0;
+            opCounter = 0;
+            clicks = [];
+            display.textContent = finalResult;
+        break;
     }
 }
-console.table(a, op, b);
